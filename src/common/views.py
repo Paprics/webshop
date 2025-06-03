13956a2b759a28e635d87fb8d2e14030f8c2834b
Valeriy <paprics@gmail.com>
2025-06-03 12:54:51 +0300

    Add base template

--- src/common/views.py
+++ src/common/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render  # noqa F401
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+    extra_context = {"title": "Home Page | Домашня сторінка"}
