fa2a4949db1535cf532d91747aec3df2c91de7d0
Valeriy <paprics@gmail.com>
2025-06-09 19:14:47 +0300

    Add member registration functionality (form, view, template)

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,9 +1,27 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.views import LogoutView, LoginView
 from django.shortcuts import redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.views.generic import FormView
+
+from accounts.rorms import MemberCreationForm
 
 
+class RegisterView(FormView):
+    template_name = 'registration/register.html'
+    form_class = MemberCreationForm
+    success_url = reverse_lazy('common:index')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('common:index'))
+        return super().dispatch(request, *args, **kwargs)
 
 class LoginView(LoginView):
     template_name = 'registration/login.html'
@@ -12,8 +30,6 @@ class LoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('common:index')
-
-
 
 class LogoutView(LogoutView):
     next_page = 'login'
