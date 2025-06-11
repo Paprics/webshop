8c6f47880e96b5ae265005c00b24f980d8039b02
Valeriy <paprics@gmail.com>
2025-06-11 23:10:43 +0300

    Added basic product detail page draft

--- src/store/views.py
+++ src/store/views.py
@@ -13,10 +13,11 @@ class ProductsListView(ListView):
 class ProductDetailView(DetailView):
     model = models.ProductModel
     template_name = "product_detail.html"
+    context_object_name = "product"
 
     # По умолчанию DetailView ищет объект по pk, чтобы искать по slug:
     slug_field = "slug"
-    slug_url_kwarg = "slug"
+    slug_url_kwarg = "slug_product"
 
 
 class ProductsCategoryView(ListView):
