a2b18bcbe2385aa9d5a42ef8e87c4775769e2fc4
Valeriy <paprics@gmail.com>
2025-06-13 12:19:45 +0300

    Add Docker support for project containerization

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -11,9 +11,7 @@ INSTALLED_APPS += [  # noqa: F405
     "debug_toolbar",
 ]
 
-MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    ]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 
 DATABASES = {
     "default": {
