a2603029809720225c2155d32c0f017ad56816c2
Valeriy <paprics@gmail.com>
2025-06-19 22:32:01 +0300

    Add cart icon badge showing number of items

--- src/common/context_processors.py
+++ src/common/context_processors.py
@@ -1,13 +1,13 @@
-def categories_processor(request):
-    from store.models import CategoryModel
-
-    categories = CategoryModel.objects.all()
-    return {"categories": categories}
-
+from cart.views import get_cart
 
 def category_processor_nested(request):
     from store.models import CategoryModelMPTT
 
     # categories_nested = CategoryModelMPTT.objects.root_nodes()  # N+1 query
     categories_nested = CategoryModelMPTT.objects.root_nodes().prefetch_related("children")  # TODO: check optimiz.
-    return {"categories_nested": categories_nested}
+    cart = get_cart(request)
+    cart_items_count = sum(item.quantity for item in cart.items)
+    return {
+        "categories_nested": categories_nested,
+        "cart_items_count": cart_items_count,
+    }
