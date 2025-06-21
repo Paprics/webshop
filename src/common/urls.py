21820bc7476ff8633e2915671cb840d22713f779
Valeriy <paprics@gmail.com>
2025-06-21 17:02:20 +0300

    Update HTML structure

--- src/common/urls.py
+++ src/common/urls.py
@@ -7,11 +7,10 @@ app_name = "common"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("about/", views.AboutView.as_view(), name="about"),
-    path("contacts/", views.ContactView.as_view(), name="contacts"),
-    path("fqa/", views.FaqView.as_view(), name="faq"),
-    path("page404/", views.Page404View.as_view(), name="page404"),
+    path("about/", TemplateView.as_view(template_name="about.html"), name="about"),
+    path("contacts/", TemplateView.as_view(template_name="contacts.html"), name="contacts"),
+    path("fqa/", TemplateView.as_view(template_name="faq.html"), name="faq"),
+    path("page404/", TemplateView.as_view(template_name="page404.html"), name="page404"),
     path("feedback/", views.Feedback.as_view(), name="feedback"),
-    path("settings/", TemplateView.as_view(template_name="settings.html"), name="settings"),
     path("customer_detail/", views.CustomerDetailView.as_view(), name="customer_detail"),
 ]
