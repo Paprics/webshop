cec78630e8c70acd9924b12b29c6ea2946cb8c24
Valeriy <paprics@gmail.com>
2025-06-21 14:03:25 +0300

    Add view and template for password change

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -28,7 +28,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
     }
 else:
     DATABASES = {
-        "default_sqlite": {
+        "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
         },
