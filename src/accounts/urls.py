8cb610035ecc9e2e77bde6657592f513fb2a616d
Valeriy <paprics@gmail.com>
2025-06-21 18:10:17 +0300

    Updated personal cabinet logic and customer edit flow

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("delete_account/", views.DeleteAccountView.as_view(), name="delete_account"),
     path("change_password/", views.PassChangeView.as_view(), name="change_password"),
+    path("update_customer/", views.UpdateCustomerView.as_view(), name="update_customer"),
+    path("update_delyvery/", views.UpdateDelyAddressView.as_view(), name="update_delyvery"),
 ]
