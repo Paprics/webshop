a7d3adde83b08688d9c8439474774279297d365b
Valeriy <paprics@gmail.com>
2025-06-24 15:00:06 +0300

    Add basic search functionality — not final

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS += [  # noqa: F405
     "django_extensions",
