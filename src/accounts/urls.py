24e7024601552c29b0d2286a64b0e650667aa241
Valeriy <paprics@gmail.com>
2025-07-12 11:34:29 +0300

    Add user registration with email confirmation

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -7,9 +8,14 @@ app_name = "accounts"
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
-    path("register/", views.RegisterView.as_view(), name="register"),
+    path("register/", views.RegistrationView.as_view(), name="register"),
     path("delete_account/", views.DeleteAccountView.as_view(), name="delete_account"),
     path("change_password/", views.PassChangeView.as_view(), name="change_password"),
     path("update_customer/", views.UpdateCustomerView.as_view(), name="update_customer"),
     path("update_delyvery/", views.UpdateDelyAddressView.as_view(), name="update_delyvery"),
+    path('activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name='activate_account'),
+    path('activate/success/', TemplateView.as_view(template_name='activation_success.html'), name='activation_success'),
+    path('activate/failed/', TemplateView.as_view(template_name='activation_failed.html'), name='activation_failed'),
+    path("activate/info/", TemplateView.as_view(template_name='activation_massage.html'), name='activation_massage'),
+
 ]
---
+++ src/accounts/utils/utils.py
@@ -0,0 +1,40 @@
+#utils.py
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+
+
+class TokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return f"{user.pk}{timestamp}{user.is_active}"
+
+
+def send_registration_mail(customer, request) -> None:
+
+    uidb64 = urlsafe_base64_encode(force_bytes(customer.id))
+    token = TokenGenerator().make_token(customer)
+    domain = get_current_site(request).domain
+    activation_link = f"http://{domain}/activate/{uidb64}/{token}/"
+
+    message = render_to_string(
+        "activation_email.html",
+        {
+            "customer": customer,
+            "activation_link": activation_link,
+        },
+    )
+
+    email = EmailMessage(
+        subject="Активація облікового запису",
+        body=message,
+        to=[customer.email],
+    )
+
+    email.content_subtype = "html"
+    email.send(fail_silently=True)
+
