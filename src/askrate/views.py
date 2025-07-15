f68bd6294bcc3946429801bd8e2bcb4cada2ccf1
Valeriy D. <paprics@gmail.com>
2025-07-15 23:39:57 +0300

    review

--- src/askrate/views.py
+++ src/askrate/views.py
@@ -8,9 +8,11 @@ class AskRateCreateView(CreateView):
     model = AskRateModel
     form_class = AskRateForms
 
-    def get_success_url(self):
-        return self.request.META.get("HTTP_REFERER", "/")
-
     def form_valid(self, form):
         form.instance.customer = self.request.user if self.request.user.is_authenticated else None
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.request.META.get("HTTP_REFERER", "/")
+
+
